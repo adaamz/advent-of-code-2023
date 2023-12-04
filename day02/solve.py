12f94b9d043bae7093c88fd89f8684d106ac78bf
Adam Žurek <adam.zurek@threatmark.com>
2023-12-04 19:28:40 +0100

    day2 part2

--- day02/solve.py
+++ day02/solve.py
@@ -1,4 +1,5 @@
 import re
+from math import prod
 
 limit = {"red": 12, "green": 13, "blue": 14}
 
@@ -26,3 +27,30 @@ def solve_part1(input: str) -> int:
             sum += game_id
 
     return sum
+
+
+def _maximum_rounds(rounds: list[str]) -> dict[str, int]:
+    maximum = {color: 0 for color in ["blue", "green", "red"]}
+
+    for round in rounds:
+        bags = round.split(", ")
+        for bag in bags:
+            count, color = bag.split(" ")
+            maximum[color] = max(maximum[color], int(count))
+
+    return maximum
+
+
+def solve_part2(input: str) -> int:
+    game_id_re = re.compile("^Game (\d+):")
+
+    result = 0
+
+    for line in input.splitlines():
+        game_id = int(game_id_re.findall(line)[0])
+
+        rounds = line.removeprefix(f"Game {game_id}: ").split("; ")
+
+        result += prod(_maximum_rounds(rounds).values())
+
+    return result
--- day02/test.py
+++ day02/test.py
@@ -1,4 +1,5 @@
 from day02.solve import solve_part1
+from day02.solve import solve_part2
 
 
 def test_example1() -> None:
@@ -15,3 +16,19 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 def test_final1() -> None:
     with open("day02/final.txt", "r") as f:
         assert solve_part1(f.read()) == 2265
+
+
+def test_example2() -> None:
+    example = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
+
+    assert solve_part2(example) == 2286
+
+
+def test_final2() -> None:
+    with open("day02/final.txt", "r") as f:
+        assert solve_part2(f.read()) == 64097
