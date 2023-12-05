e91c135a015c8d6485fb0ed2590de8c46fd6c0da
Adam Žurek <adam.zurek@threatmark.com>
2023-12-05 09:04:44 +0100

    day3 part2

--- day03/solve.py
+++ day03/solve.py
@@ -1,3 +1,8 @@
+from collections import defaultdict
+from math import prod
+from typing import Optional
+
+
 def solve_part1(input: str) -> int:
     lines = input.splitlines()
 
@@ -39,3 +44,51 @@ def solve_part1(input: str) -> int:
                 end_numbers = None
 
     return sum(not_abandoned_numbers)
+
+
+def solve_part2(input: str) -> int:
+
+    lines = input.splitlines()
+
+    def _find_star(line_id: int, start_numbers_index: int, end_numbers_index: int) -> Optional[tuple[int, int]]:
+        for y in range(max(line_id - 1, 0), min(len(lines) - 1, line_id + 1) + 1):
+            for x in range(max(start_numbers_index - 1, 0), min(len(lines[line_id]) - 1, end_numbers_index + 1) + 1):
+                if lines[y][x] == "*":
+                    return y, x
+
+        return None
+
+    numbers_with_star = defaultdict(list)
+
+    for y, line in enumerate(lines):
+        start_numbers = None
+        end_numbers = None
+
+        for x, c in enumerate(line):
+            if c.isdigit() or (x == len(line) - 1 and start_numbers is not None and end_numbers is None):
+                if start_numbers is None:
+                    start_numbers = x
+                else:
+                    end_numbers = x
+
+                    if x == len(line) - 1:
+                        if (star := _find_star(y, start_numbers, end_numbers)) is not None:
+                            number = line[start_numbers: end_numbers + 1]
+                            numbers_with_star[f"{star[0]}-{star[1]}"].append(int("".join(number)))
+            else:
+                if start_numbers is not None and end_numbers is None:
+                    end_numbers = x-1
+
+                if start_numbers is not None and end_numbers is not None:
+                    if (star := _find_star(y, start_numbers, end_numbers)) is not None:
+                        number = line[start_numbers: end_numbers + 1]
+                        numbers_with_star[f"{star[0]}-{star[1]}"].append(int("".join(number)))
+
+                start_numbers = None
+                end_numbers = None
+
+    has_two_numbers = filter(
+        lambda numbers: len(numbers) == 2, numbers_with_star.values()
+    )
+    product_numbers = map(prod, has_two_numbers)
+    return sum(product_numbers)
--- day03/test.py
+++ day03/test.py
@@ -1,4 +1,5 @@
 from day03.solve import solve_part1
+from day03.solve import solve_part2
 
 
 def test_example1() -> None:
@@ -35,3 +36,24 @@ def test_final1() -> None:
     with open("day03/final.txt", "r") as f:
         result = solve_part1(f.read())
         assert result == 539590
+
+
+def test_example2() -> None:
+    example = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
+
+    assert solve_part2(example) == 467835
+
+
+def test_final2() -> None:
+    with open("day03/final.txt", "r") as f:
+        result = solve_part2(f.read())
+        assert result == 80703636
